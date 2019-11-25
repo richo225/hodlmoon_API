83540140589db3693eec2930dbc8e2fe9f2ffddc
Richard Bates <richo225@gmail.com>
2019-11-25 04:48:47 +0000

    Extend User model fix for updated version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :trackable, :validatable,
-                    :confirmable, :omniauthable
-            include DeviseTokenAuth::Concerns::User
+  extend Devise::Models
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
+
   has_one :portfolio
   has_many :orders
 end
