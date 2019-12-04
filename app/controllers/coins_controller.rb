3d9ef89b570d792c08415309ab927d268d460a47
Richard Bates <richo225@gmail.com>
2019-12-04 21:53:48 +0000

    Get rid of jsonapi-resources (it sucks)

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,2 +1,20 @@
-class CoinsController < JSONAPI::ResourceController
+class CoinsController < ApplicationController
+  before_action :set_coin, only: [:show]
+
+  # GET /coins
+  def index
+    @coins = Coin.order(:cmc_rank)
+    render json: CoinSerializer.new(@coins).serialized_json
+  end
+
+  # GET /coins/1
+  def show
+    render json: CoinSerializer.new(@coin).serialized_json
+  end
+
+  private
+
+  def set_coin
+    @coin = Coin.find(params[:id])
+  end
 end
