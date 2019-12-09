c01fdc8eb0cd6866a90ad30d90753e53a30e741c
Richard Bates <richo225@gmail.com>
2019-12-09 16:12:09 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+return unless Rails.env.production?
+
+puts 'seeding exchanges.......'
 Rake::Task['exchanges:seed_db'].invoke
+
+puts 'seeding coins...........'
 Rake::Task['coins:seed_db'].invoke
+
+puts 'attaching coin icons via S3...........'
 Rake::Task['coin_icons:attach_files'].invoke
