7c433686798b7f2e343c410916ce829fbc4b0a56
Richard Bates <richo225@gmail.com>
2019-12-10 17:45:44 +0000

    Update asset host location

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module HodlmoonApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.action_mailer.asset_host = ENV.fetch('ROOT_URL')
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
